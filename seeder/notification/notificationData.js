8f74339a9d94aaa333888bc1e719f4a532c37f72
shahiqshahzad <shahiqshaikh123@gmail.com>
2023-09-05 18:59:45 +0500

    get-product

---
+++ seeder/notification/notificationData.js
@@ -0,0 +1,28 @@
+const ProductData = [
+  {
+    name: "Iphone x",
+    description: "this is iphone x",
+    price: 120000,
+    stock: 12,
+  },
+  {
+    name: "Iphone 11",
+    description: "this is iphone 11",
+    price: 160000,
+    stock: 12,
+  },
+  {
+    name: "Iphone 12",
+    description: "this is iphone 12",
+    price: 220000,
+    stock: 12,
+  },
+  {
+    name: "Iphone 13",
+    description: "this is iphone 13",
+    price: 320000,
+    stock: 12,
+  },
+];
+
+export default ProductData;
